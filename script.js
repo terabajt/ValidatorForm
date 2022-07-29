4eee3c8a062d4a8f6b89f9ab13cd9c399ae48d51
Michał Pasynek <michalpasynek@gmail.com>
2022-07-29 13:30:46 +0200

    Create CLEAR JS function

---
+++ script.js
@@ -0,0 +1,14 @@
+const username = document.querySelector('#username')
+const pass = document.querySelector('#password')
+const pass2 = document.querySelector('#password2')
+const email = document.querySelector('#email')
+const sendBtn = document.querySelector('.send')
+const clearBtn = document.querySelector('.clear')
+const popupBtn = document.querySelector('.popup')
+
+clearBtn.addEventListener('click', e => {
+	e.preventDefault();
+    [username, pass, pass2, email].forEach(el => {
+		el.value = '';
+	})
+})
